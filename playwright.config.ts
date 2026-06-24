60da0d78025620bf17edd56efd89ed8a74f0d352
haritha <haritharavichandran11@gmail.com>
2026-06-24 21:04:23 +0530

    allure added

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -40,7 +43,7 @@ export default defineConfig({
     headless:true,
     screenshot:'only-on-failure',
     video:'retain-on-failure',
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
